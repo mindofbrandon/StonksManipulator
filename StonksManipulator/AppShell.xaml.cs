f88df27802f5b5ab02c50da41111970ce48b8257
Brandon Zuniga <brandon_zuniga@student.uml.edu>
2021-06-26 11:38:09 -0400

    Add project files.

---
+++ StonksManipulator/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using StonksManipulator.ViewModels;
+using StonksManipulator.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace StonksManipulator
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ StonksManipulator/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using StonksManipulator.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace StonksManipulator.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
